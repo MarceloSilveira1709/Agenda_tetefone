9fa03290c521fba9620d18323577ea81843ad83e
MarceloSilveira <marcelosilverag@hotmail.com>
2024-01-19 11:44:18 -0300

    fix:alinhamento campo contato

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
-const form = document.getElementById('form-atividade');
-let linhas = '';
-
-form.addEventListener('submit', function(e) {
+    const form = document.getElementById('form-atividade');
+    let linhas = '';
+    form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-const inputNomeContato = document.getElementById('nome-contato');
-const inputNumeroContato = document.getElementById('numero-contato');
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
 
-let linha = '<tr>';
-linha += `<td>${inputNomeContato.value}<td>`;
-linha += `<td>${inputNumeroContato.value}<td>`;
-linha += '<td>';
+    let linha = '<tr>';
 
-linhas += linha;
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+    linhas += linha;
 
-const corpoTabela = document.querySelector('tbody');
-corpoTabela.innerHTML = linhas;
-
-
-});
-
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+    });
